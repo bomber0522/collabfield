cff8f13336b809ba0c1aef6f9c19f5291125467c
Takaki <takaki55730317@gmail.com>
2019-12-11 16:50:01 +0900

    Create a UI to create new posts - Inside the PostsController: define new and create actions define a post_params method define a before_action filter - Inside the ApplicationController: define a redirect_if_not_signed_in method define a redirext_if_signed_in method - Create a new template for posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,5 @@
 class PostsController < ApplicationController
+  before_action :redirect_if_not_signed_in, only: [:new]
 
   def index
     @posts = Post.limit(5)
@@ -20,6 +21,21 @@ class PostsController < ApplicationController
     posts_for_branch(params[:action])
   end
 
+  def new
+    @branch = params[:branch]
+    @categories = Category.where(branch: @branch)
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to post_path(@post)
+    else
+      redirect_to root_path
+    end
+  end
+
   private
 
   def posts_for_branch(branch)
@@ -37,5 +53,9 @@ class PostsController < ApplicationController
       category: params[:category],
       branch: params[:action]
       }).call
+  end
+
+  def post_params
+    params.require(:post).permit(:content, :title, :category_id).merge(user_id: current_user.id)
   end
 end
