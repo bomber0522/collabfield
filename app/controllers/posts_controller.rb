c247a58992cf5070e2e97b982cb6072fea86db2a
Takaki <takaki55730317@gmail.com>
2019-12-11 14:05:46 +0900

    Transform posts pagination into infinite scroll - Create an infinite_scroll.js file - Inside PostController's posts_for_branch method add respond_to format - Define an update_pagination_partial_path - Create _update_pagination.js.erb and _remove_pagination.js.erb partials - hide the .infinite-scroll element with CSS

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -25,6 +25,10 @@ class PostsController < ApplicationController
   def posts_for_branch(branch)
     @categories = Category.where(branch: branch)
     @posts = get_posts.paginate(page: params[:page])
+    respond_to do |format|
+      format.html
+      format.js { render partial: 'posts/posts_pagination_page' }
+    end
   end
 
   def get_posts
@@ -36,7 +40,7 @@ class PostsController < ApplicationController
       posts = Post.by_branch(branch).all
     elsif category.blank? && search.present?
       posts = Post.by_branch(branch).search(search)
-    elsif categroy.present? && search.blank?
+    elsif category.present? && search.blank?
       posts = Post.by_category(branch, category)
     elsif category.present? && search.present?
       posts = Post.by_category(branch, category).search(search)
